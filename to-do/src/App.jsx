e5d22405d99754f6d419cec110e50c581b7f5293
nicas <nica.sergiu@seznam.cz>
2024-05-22 19:27:46 +0100

    first commit

---
+++ to-do/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+
+function App() {
+  const [toDoList, setToDoList] = useState([])
+  const [newTask, setNewTask] = useState("")
+
+  const writeTask = (event) =>{
+    setNewTask(event.target.value)
+  }
+
+  const addTask = () =>{
+    const newTodoList = [...toDoList, newTask]
+    setToDoList(newTodoList)
+  }
+
+  const deletTask = (taskName) =>{
+    setToDoList(toDoList.filter((task) => task !== taskName));
+  }
+
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input onChange={writeTask}></input>
+        <button onClick={addTask}>Press me</button>
+      </div>
+      <div className="list">
+        {toDoList.map((task)=>{
+          return <div>
+            <h1>{task}</h1>
+            <button onClick={() => deletTask(task)}>Delete</button>
+            </div>
+
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App
